7d5fd6e945171587e0d227070fba91d9ac3a78c6
Restuta <restuta8@gmail.com>
2015-02-20 12:05:39 -0800

    adding bower components folder

---
+++ bower_components/particles.js/demo/js/app.js
@@ -0,0 +1,68 @@
+/* default dom id (particles-js) */
+//particlesJS();
+
+/* config dom id */
+//particlesJS('dom-id');
+
+/* config dom id (optional) + config particles params */
+particlesJS('particles-js', {
+  particles: {
+    color: '#fff',
+    color_random: false,
+    shape: 'circle', // "circle", "edge" or "triangle"
+    opacity: {
+      opacity: 1,
+      anim: {
+        enable: true,
+        speed: 1.5,
+        opacity_min: 0,
+        sync: false
+      }
+    },
+    size: 4,
+    size_random: true,
+    nb: 150,
+    line_linked: {
+      enable_auto: true,
+      distance: 100,
+      color: '#fff',
+      opacity: 1,
+      width: 1,
+      condensed_mode: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 600
+      }
+    },
+    anim: {
+      enable: true,
+      speed: 1
+    }
+  },
+  interactivity: {
+    enable: true,
+    mouse: {
+      distance: 300
+    },
+    detect_on: 'canvas', // "canvas" or "window"
+    mode: 'grab',
+    line_linked: {
+      opacity: .5
+    },
+    events: {
+      onclick: {
+        enable: true,
+        mode: 'push', // "push" or "remove"
+        nb: 4
+      },
+      onresize: {
+        enable: true,
+        mode: 'out', // "out" or "bounce"
+        density_auto: false,
+        density_area: 800 // nb_particles = particles.nb * (canvas width *  canvas height / 1000) / density_area
+      }
+    }
+  },
+  /* Retina Display Support */
+  retina_detect: true
+});
