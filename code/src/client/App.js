033e6f8d8e00b697a10e4ba5eec352fe0809715b
Restuta <restuta8@gmail.com>
2015-10-18 23:40:57 -0700

    big cleanup after font selection

--- code/src/client/App.js
+++ code/src/client/App.js
@@ -22,129 +22,34 @@ class Counter extends Component {
 
   render() {
     return (
-      <h2 style={{ color: this.props.color }}>
-        Counter ({this.props.increment}): {this.state.counter}
-      </h2>
+      <h3 style={{ color: this.props.color, marginTop: this.props.marginTop }}>
+        Last full reload ({this.props.increment}): <mark>{this.state.counter}</mark> seconds ago
+      </h3>
     );
   }
 }
 
-const Stateless = (props) => {
-//  shouldComponentUpdate = shouldPureComponentUpdate;
-  const {name} = props;
-
-  return <div>{name}</div>;
-};
-
 
 export class App extends Component { // eslint-disable-line react/no-multi-comp
-
-
   render() {
-    const fontHeadingStyle = {
-        backgroundColor: 'pink',
-        paddingLeft: '15px',
-        marginLeft: '-13px',
-        marginRight: '-13px'
-    };
-
-    const headerText = 'Road Races in CA, 100mi range'
-      .toUpperCase();
-
     return (
       <div>
         <div className="container">
           <div className="row">
             <div className="col-sm-12 transparent">
-              <h1 className="oswald">{headerText}</h1>
-              <h1 className="montserrat">{headerText}</h1>
-              <h1 className="merriweather">{headerText}</h1>
-              <h1 className="francois-one">{headerText}</h1>
-              <h1 className="aleo">{headerText}</h1>
+              <h1 className="oswald">Road Races in CA, 100mi range</h1>
             </div>
           </div>
           <div className="row">
-          <div className="col-sm-2 source-sans-pro">
-            <div style={fontHeadingStyle}>Source Sans Pro (H3 A2)</div>
-              Dunnigan Hills Road Race
-              64mi, 2h, 6200ft
-              1234567890
-              QWERTYqwerty
-              lb dl 6b illb
-              Good job RCN rcn!
-              Content long one
-              Content long one
-              Content long one
-            </div>
-            <div className="col-sm-2">
-              <div style={fontHeadingStyle}>Lato (H2 A1)</div>
-              Dunnigan Hills Road Race
-              64mi, 2h, 6200ft
-              1234567890
-              QWERTYqwerty
-              lb dl 6b illb
-              Good job RCN rcn!
-              Content long one
-              Content long one
-              Content long one
-            </div>
-            <div className="col-sm-2 roboto-condensed">
-              <div style={fontHeadingStyle}>Roboto Condensed</div>
-              Dunnigan Hills Road Race
-              64mi, 2h, 6200ft
-              1234567890
-              QWERTYqwerty
-              lb dl 6b illb
-              Good job RCN rcn!
-              Content long one
-              Content long one
-              Content long one
-            </div>
-            <div className="col-sm-2 alegreya-sans">
-              <div style={fontHeadingStyle}>Alegreya Sans (H1 A3)</div>
-              Dunnigan Hills Road Race
-              64mi, 2h, 6200ft
-              1234567890
-              QWERTYqwerty
-              lb dl 6b illb
-              Good job RCN rcn!
-              Content long one
-              Content long one
-              Content long one
-            </div>
-            <div className="col-sm-2 open-sans">
-              <div style={fontHeadingStyle}>Open Sans (H3 A4)</div>
-              Dunnigan Hills Road Race
-              64mi, 2h, 6200ft
-              1234567890
-              QWERTYqwerty Zz
-              lb dl 6b illb
-              Good job RCN rcn!
-              Content long one
-              Content long one
-              Content long one
-            </div>
-            <div className="col-sm-2 roboto">
-              <div style={fontHeadingStyle}>Roboto</div>
-              Dunnigan Hills Road Race
-              64mi, 2h, 6200ft
-              1234567890
-              QWERTYqwerty Zz
-              lb dl 6b illb
-              Good job RCN rcn!
-              Content long one
-              Content long one
-              Content long one
-            </div>
-
+            <div className="col-sm-2">1</div>
+            <div className="col-sm-2">2</div>
+            <div className="col-sm-2">3</div>
+            <div className="col-sm-2">4</div>
+            <div className="col-sm-2">5</div>
+            <div className="col-sm-2">6</div>
           </div>
         </div>
-        <RowPure name="Pure Cat"/>
-
-        <Stateless name="Chandler" />
-        <Counter increment={1} color="blueviolet" />
-        <Counter increment={5} color="lightblue" />
-        <Counter increment={3} color="lightgreen" />
+        <Counter increment={1} color="silver" marginTop="20px" />
       </div>
     );
   }
