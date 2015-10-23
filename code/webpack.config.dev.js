4720858f64b3fa39c6e7d1ae23099f4f354ae99b
Restuta <restuta8@gmail.com>
2015-10-22 22:34:38 -0700

    removed web pack notifications plugin

--- code/webpack.config.dev.js
+++ code/webpack.config.dev.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const webpack = require('webpack');
-const WebpackNotifierPlugin = require('webpack-notifier');
 const constants = require('./constants');
 
 module.exports = {
