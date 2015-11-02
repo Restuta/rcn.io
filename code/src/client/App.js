66fffe5a27942292830fca560a32df4a3614f939
Restuta <restuta8@gmail.com>
2015-11-02 13:57:41 -0800

    * moved counter to tmp/ folder * set BS columns to 14

--- code/src/client/App.js
+++ code/src/client/App.js
@@ -1,40 +1,16 @@
 import React from 'react';
 import Component from 'react-pure-render/component';
 import './app.scss';
-import Row from './atoms/Row.jsx'
+import Row from './atoms/Row.jsx';
+import Counter from './tmp/Counter.jsx';
 
-class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { counter: 0 };
-    this.interval = setInterval(() => this.tick(), 1000);
-  }
-
-  tick() {
-    this.setState({
-      counter: this.state.counter + this.props.increment
-    });
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
-  render() {
-    return (
-      <h3 style={{ color: this.props.color, marginTop: this.props.marginTop }}>
-        Last full reload: <mark>{this.state.counter}</mark> seconds ago
-      </h3>
-    );
-  }
-}
 
 
 export class App extends Component { // eslint-disable-line react/no-multi-comp
   render() {
     let columns = [];
-    for (let i = 1; i <= 120; i++) {
-      columns.push(<div className="col-sm-1">{i}</div>);
+    for (let i = 1; i <= 31; i++) {
+      columns.push(<div key={i} className="col-sm-2">{i}</div>);
     }
 
     return (
@@ -42,7 +18,7 @@ export class App extends Component { // eslint-disable-line react/no-multi-comp
         <div className="container">
           <div className="row">
             <div className="col-sm-12 transparent">
-              <h1 className="oswald">Road Races in CA, 100mi range</h1>
+              <h1 className="oswald">Road Races in CA, 100mi range </h1>
             </div>
           </div>
           <Row className="bla">
