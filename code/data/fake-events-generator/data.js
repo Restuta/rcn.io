6fe78dca867eb37af044aa62b4c9b69ff9649a6a
Restuta <restuta8@gmail.com>
2015-11-03 09:07:42 -0800

    moved "data" to a separate module

---
+++ code/data/fake-events-generator/data.js
@@ -0,0 +1,228 @@
+const eventData = [{
+  'date': 'January 31st 2015',
+  'name': 'Cal Aggie Criterium'
+}, {
+  'date': 'February 7th 2015',
+  'name': 'John C. Schlesinger Memorial Circuit Race and Team Time Trial'
+}, {
+  'date': 'February 8th 2015',
+  'name': 'RED KITE OMNIUM EVENT #1 - THE BUMP CIRCUIT RACE (WINTER)'
+}, /*{
+  'date': 'February 8th 2015',
+  'name': 'Christopher Castro Memorial Road Race '
+}, {
+  'date': 'February 14th 2015',
+  'name': 'Folsom Winter Criterium'
+}, {
+  'date': 'February 15th 2015',
+  'name': 'Pine Flat Road Race'
+}, {
+  'date': 'February 16th 2015',
+  'name': 'Dinuba Criterium'
+}, {
+  'date': 'February 21st 2015',
+  'name': 'Snelling Road Race'
+}, {
+  'date': 'February 21st 2015',
+  'name': 'UC Santa Cruz Slugapalooza'
+}, {
+  'date': 'February 22nd 2015',
+  'name': 'Original Merced Criterium'
+}, {
+  'date': 'February 27th 2015',
+  'name': 'Chico Stage Race p/b Sierra Nevada Brewing Co.'
+}, {
+  'date': 'March 6th 2015',
+  'name': 'Madera County Stage Race'
+}, {
+  'date': 'March 8th 2015',
+  'name': 'RED KITE OMNIUM EVENT #2 - RED KITE CRITERIUM (MARCH)'
+}, {
+  'date': 'March 14th 2015',
+  'name': 'Wards Ferry Road Race'
+}, {
+  'date': 'March 21st 2015',
+  'name': 'UC Merced Race Weekend'
+}, {
+  'date': 'March 22nd 2015',
+  'name': 'RED KITE OMNIUM EVENT #3 - TEMPUS FUGIT INDIVIDUAL TIME TRIAL (SP'
+}, {
+  'date': 'March 22nd 2015',
+  'name': 'Chowcilla Criterium'
+}, {
+  'date': 'March 28th 2015',
+  'name': 'Fresno State Road Race Weekend'
+}, {
+  'date': 'March 28th 2015',
+  'name': 'Turlock lake Road Race'
+}, {
+  'date': 'March 29th 2015',
+  'name': 'Santa Cruz Classic Criterium'
+}, {
+  'date': 'April 4th 2015',
+  'name': 'Copperopolis Road Race'
+}, {
+  'date': 'April 19th 2015',
+  'name': 'RED KITE OMNIUM EVENT #4 - RED KITE CRITERIUM (APRIL)'
+}, {
+  'date': 'April 25th 2015',
+  'name': 'WCCC Championships p/b Cal Cycling'
+}, {
+  'date': 'April 25th 2015',
+  'name': 'Clovis Hills Criterium'
+}, {
+  'date': 'April 26th 2015',
+  'name': 'Budweiser Sequoia Cycling Classic'
+}, {
+  'date': 'April 26th 2015',
+  'name': 'Wente Vineyards Classic Road Race'
+}, {
+  'date': 'May 2nd 2015',
+  'name': 'Mike'
+  s Bikes 42 nd Cat’ s Hill Classic '
+}, {
+  'date': 'May 2nd 2015',
+  'name': 'Golden State Race Series'
+}, {
+  'date': 'May 3rd 2015',
+  'name': 'Salinas Criterium'
+}, {
+  'date': 'May 3rd 2015',
+  'name': 'Calaveras Time Trial '
+}, {
+  'date': 'May 8th 2015',
+  'name': 'Tour of California Women'
+  s Edition '
+}, {
+  'date': 'May 8th 2015',
+  'name': 'Honor Ride Lake Tahoe'
+}, {
+  'date': 'May 9th 2015',
+  'name': 'Berkeley Hills Road Race'
+}, {
+  'date': 'May 10th 2015',
+  'name': 'RED KITE OMNIUM EVENT #6 - RED KITE CRITERIUM (MAY)'
+}, {
+  'date': 'May 15th 2015',
+  'name': 'Mariposa Women'
+  s Stage Race '
+}, {
+  'date': 'May 23rd 2015',
+  'name': 'Death Valley Road Omnium'
+}, {
+  'date': 'May 23rd 2015',
+  'name': 'Folsom Classic Criterium'
+}, {
+  'date': 'May 25th 2015',
+  'name': 'Memorial Day Criterium'
+}, {
+  'date': 'May 30th 2015',
+  'name': 'IC3 Dash for Cash'
+}, {
+  'date': 'May 31st 2015',
+  'name': 'RED KITE OMNIUM EVENT #7 - THE BUMP CIRCUIT RACE (SPRING)'
+}, {
+  'date': 'June 6th 2015',
+  'name': 'Pescadero Coastal Classic & Elite District Championship'
+}, {
+  'date': 'June 20th 2015',
+  'name': 'RED KITE OMNIUM EVENT #8 - TRI-VALLEY CRITERIUM CLASSIC'
+}, {
+  'date': 'June 21st 2015',
+  'name': '55th Annual Nevada City Bicycle Classic'
+}, {
+  'date': 'December 1st 2015',
+  'name': '2015 USA Cycling Amateur Road Nationals'
+}, {
+  'date': 'June 28th 2015',
+  'name': 'The Parnassus Investments Burlingame Criterium'
+}, {
+  'date': 'July 4th 2015',
+  'name': 'Davis Bike Club 4th of July Criterium'
+}, {
+  'date': 'July 5th 2015',
+  'name': ' Lodoga Road Race (Formerly Leesville)'
+}, {
+  'date': 'July 11th 2015',
+  'name': 'Northern CA/NV District Championships'
+}, {
+  'date': 'July 12th 2015',
+  'name': 'RED KITE OMNIUM EVENT #9 - RED KITE CRITERIUM (JULY)'
+}, {
+  'date': 'July 25th 2015',
+  'name': 'San Rafael Twilight Criterium'
+}, {
+  'date': 'July 26th 2015',
+  'name': 'RED KITE OMNIUM EVENT #11 - 31st ANNUAL BERKELEY BICYCLE CLUB CRITERIU'
+}, {
+  'date': 'August 9th 2015',
+  'name': 'RED KITE OMNIUM EVENT #12 - RED KITE CRITERIUM (AUGUST)'
+}, {
+  'date': 'August 15th 2015',
+  'name': 'Dunnigan Hills Road Race'
+}, {
+  'date': 'August 23rd 2015',
+  'name': 'RED KITE OMNIUM EVENT #13 - TEMPUS FUGIT INDIVIDUAL TIME TRIAL (S'
+}, {
+  'date': 'August 23rd 2015',
+  'name': 'University Road Race'
+}, {
+  'date': 'August 29th 2015',
+  'name': 'Winters Road Race'
+}, {
+  'date': 'August 30th 2015',
+  'name': 'Vacaville Gran Prix'
+}, {
+  'date': 'September 5th 2015',
+  'name': 'RED KITE CHAMPIONSHIP WEEKEND - RED KITE OMNIUM AND NCNCA STATE CRITER'
+}, {
+  'date': 'September 7th 2015',
+  'name': 'Giro Di San Francisco'
+}, {
+  'date': 'September 12th 2015',
+  'name': '52nd Mt. Tamalpais Hill Climb'
+}, {
+  'date': 'September 20th 2015',
+  'name': 'Fremont Peak Hill CLimb'
+}, {
+  'date': 'September 20th 2015',
+  'name': 'Oakland Grand Prix'
+}, {
+  'date': 'September 26th 2015',
+  'name': 'Henleyville Road Race'
+}, {
+  'date': 'September 27th 2015',
+  'name': 'Esparto Time Trial'
+}, {
+  'date': 'October 10th 2015',
+  'name': 'Crush Challenge presented by Amgen'
+}, {
+  'date': 'October 11th 2015',
+  'name': 'Dylan'
+  s 2015 Ride '
+} */
+];
+
+const disciplineData = ['Road', 'MTB', 'Track', 'Cyclocross', 'Clinic'];
+const typeData = ['Road Race', 'Criterium', 'Cyclocross', 'Time Trial', 'Cross Country', 'Cross Country Marathon', 'Downhill', 'Dual Slalom', 'Short Track Cross Country', 'Super D', 'Points Race', 'Scratch Race', 'Individual Pursuit', 'Sprint', 'Keirin'];
+const timeData = ['8:00 AM', '8:30 AM', '9:00 AM', '9:20 AM', '10:00 AM', '10:15 AM', '11:40 AM', '12:00 PM', '12:03 PM', '1:00 PM', '1:55 PM', '2:00 PM', '2:18 PM'];
+const promoterData = ['2 Wheel Racing', 'Alta Alpina Cycling Club', 'Alto Velo', 'AntiGravity Cycling', 'Back of Nowhere Enterprises', 'Berkeley Bicycle Club', 'Bicycle Planet', 'Bicycles Plus',
+  'Bike Monkey', 'Bike Station Aptos', 'Brian Joder', 'Bubba Melcher', 'Cal Aggie Cycling', 'Cal Cycling', 'California Giant Cycling Team', 'CCCX (Central Coast Cyclocross)', 'Central Sierra Cyclists',
+  'Chico Corsa Cycling Club', 'China Peak Mountain Resort'
+];
+const streetData = ['Kearny Street', 'Mission Street', 'Polk Street', 'Stockton Street', 'Union Street', 'Third Street', '22nd Street', '49-Mile Scenic Drive', 'Alemany Boulevard', 'Broadway',
+  'Castro Street', 'The Embarcadero', 'Filbert Street', 'Great Highway', 'Haight Street', 'Montgomery Street', 'New Montgomery Street', 'Octavia Boulevard', 'Skyline Boulevard', 'Vermont Street', 'Howard Street'
+];
+const cityData = ['Ross', 'Sacramento', 'St. Helena', 'Salinas', 'San Anselmo', 'San Bernardino', 'San Bruno', 'San Carlos', 'San Clemente', 'San Diego', 'San Dimas', 'San Fernando', 'San Francisco', 'San Gabriel',
+  'San Jacinto', 'San Joaquin', 'San Jose', 'San Juan Bautista', 'San Juan Capistrano', 'San Leandro'
+];
+
+export default {
+  eventData,
+  disciplineData,
+  typeData,
+  timeData,
+  promoterData,
+  streetData,
+  cityData
+};
--- code/data/fake-events-generator/generator.js
+++ code/data/fake-events-generator/generator.js
@@ -1,236 +1,19 @@
 import util from 'util';
+import Data from './data';
 
-console.log('Love you, Anton ;) 🐱');
+console.log('Love you, Helen ;)');
 
-var eventData = [{
-  'date': 'January 31st 2015',
-  'name': 'Cal Aggie Criterium'
-}, {
-  'date': 'February 7th 2015',
-  'name': 'John C. Schlesinger Memorial Circuit Race and Team Time Trial'
-}, {
-  'date': 'February 8th 2015',
-  'name': 'RED KITE OMNIUM EVENT #1 - THE BUMP CIRCUIT RACE (WINTER)'
-}, /*{
-  'date': 'February 8th 2015',
-  'name': 'Christopher Castro Memorial Road Race '
-}, {
-  'date': 'February 14th 2015',
-  'name': 'Folsom Winter Criterium'
-}, {
-  'date': 'February 15th 2015',
-  'name': 'Pine Flat Road Race'
-}, {
-  'date': 'February 16th 2015',
-  'name': 'Dinuba Criterium'
-}, {
-  'date': 'February 21st 2015',
-  'name': 'Snelling Road Race'
-}, {
-  'date': 'February 21st 2015',
-  'name': 'UC Santa Cruz Slugapalooza'
-}, {
-  'date': 'February 22nd 2015',
-  'name': 'Original Merced Criterium'
-}, {
-  'date': 'February 27th 2015',
-  'name': 'Chico Stage Race p/b Sierra Nevada Brewing Co.'
-}, {
-  'date': 'March 6th 2015',
-  'name': 'Madera County Stage Race'
-}, {
-  'date': 'March 8th 2015',
-  'name': 'RED KITE OMNIUM EVENT #2 - RED KITE CRITERIUM (MARCH)'
-}, {
-  'date': 'March 14th 2015',
-  'name': 'Wards Ferry Road Race'
-}, {
-  'date': 'March 21st 2015',
-  'name': 'UC Merced Race Weekend'
-}, {
-  'date': 'March 22nd 2015',
-  'name': 'RED KITE OMNIUM EVENT #3 - TEMPUS FUGIT INDIVIDUAL TIME TRIAL (SP'
-}, {
-  'date': 'March 22nd 2015',
-  'name': 'Chowcilla Criterium'
-}, {
-  'date': 'March 28th 2015',
-  'name': 'Fresno State Road Race Weekend'
-}, {
-  'date': 'March 28th 2015',
-  'name': 'Turlock lake Road Race'
-}, {
-  'date': 'March 29th 2015',
-  'name': 'Santa Cruz Classic Criterium'
-}, {
-  'date': 'April 4th 2015',
-  'name': 'Copperopolis Road Race'
-}, {
-  'date': 'April 19th 2015',
-  'name': 'RED KITE OMNIUM EVENT #4 - RED KITE CRITERIUM (APRIL)'
-}, {
-  'date': 'April 25th 2015',
-  'name': 'WCCC Championships p/b Cal Cycling'
-}, {
-  'date': 'April 25th 2015',
-  'name': 'Clovis Hills Criterium'
-}, {
-  'date': 'April 26th 2015',
-  'name': 'Budweiser Sequoia Cycling Classic'
-}, {
-  'date': 'April 26th 2015',
-  'name': 'Wente Vineyards Classic Road Race'
-}, {
-  'date': 'May 2nd 2015',
-  'name': 'Mike'
-  s Bikes 42 nd Cat’ s Hill Classic '
-}, {
-  'date': 'May 2nd 2015',
-  'name': 'Golden State Race Series'
-}, {
-  'date': 'May 3rd 2015',
-  'name': 'Salinas Criterium'
-}, {
-  'date': 'May 3rd 2015',
-  'name': 'Calaveras Time Trial '
-}, {
-  'date': 'May 8th 2015',
-  'name': 'Tour of California Women'
-  s Edition '
-}, {
-  'date': 'May 8th 2015',
-  'name': 'Honor Ride Lake Tahoe'
-}, {
-  'date': 'May 9th 2015',
-  'name': 'Berkeley Hills Road Race'
-}, {
-  'date': 'May 10th 2015',
-  'name': 'RED KITE OMNIUM EVENT #6 - RED KITE CRITERIUM (MAY)'
-}, {
-  'date': 'May 15th 2015',
-  'name': 'Mariposa Women'
-  s Stage Race '
-}, {
-  'date': 'May 23rd 2015',
-  'name': 'Death Valley Road Omnium'
-}, {
-  'date': 'May 23rd 2015',
-  'name': 'Folsom Classic Criterium'
-}, {
-  'date': 'May 25th 2015',
-  'name': 'Memorial Day Criterium'
-}, {
-  'date': 'May 30th 2015',
-  'name': 'IC3 Dash for Cash'
-}, {
-  'date': 'May 31st 2015',
-  'name': 'RED KITE OMNIUM EVENT #7 - THE BUMP CIRCUIT RACE (SPRING)'
-}, {
-  'date': 'June 6th 2015',
-  'name': 'Pescadero Coastal Classic & Elite District Championship'
-}, {
-  'date': 'June 20th 2015',
-  'name': 'RED KITE OMNIUM EVENT #8 - TRI-VALLEY CRITERIUM CLASSIC'
-}, {
-  'date': 'June 21st 2015',
-  'name': '55th Annual Nevada City Bicycle Classic'
-}, {
-  'date': 'December 1st 2015',
-  'name': '2015 USA Cycling Amateur Road Nationals'
-}, {
-  'date': 'June 28th 2015',
-  'name': 'The Parnassus Investments Burlingame Criterium'
-}, {
-  'date': 'July 4th 2015',
-  'name': 'Davis Bike Club 4th of July Criterium'
-}, {
-  'date': 'July 5th 2015',
-  'name': ' Lodoga Road Race (Formerly Leesville)'
-}, {
-  'date': 'July 11th 2015',
-  'name': 'Northern CA/NV District Championships'
-}, {
-  'date': 'July 12th 2015',
-  'name': 'RED KITE OMNIUM EVENT #9 - RED KITE CRITERIUM (JULY)'
-}, {
-  'date': 'July 25th 2015',
-  'name': 'San Rafael Twilight Criterium'
-}, {
-  'date': 'July 26th 2015',
-  'name': 'RED KITE OMNIUM EVENT #11 - 31st ANNUAL BERKELEY BICYCLE CLUB CRITERIU'
-}, {
-  'date': 'August 9th 2015',
-  'name': 'RED KITE OMNIUM EVENT #12 - RED KITE CRITERIUM (AUGUST)'
-}, {
-  'date': 'August 15th 2015',
-  'name': 'Dunnigan Hills Road Race'
-}, {
-  'date': 'August 23rd 2015',
-  'name': 'RED KITE OMNIUM EVENT #13 - TEMPUS FUGIT INDIVIDUAL TIME TRIAL (S'
-}, {
-  'date': 'August 23rd 2015',
-  'name': 'University Road Race'
-}, {
-  'date': 'August 29th 2015',
-  'name': 'Winters Road Race'
-}, {
-  'date': 'August 30th 2015',
-  'name': 'Vacaville Gran Prix'
-}, {
-  'date': 'September 5th 2015',
-  'name': 'RED KITE CHAMPIONSHIP WEEKEND - RED KITE OMNIUM AND NCNCA STATE CRITER'
-}, {
-  'date': 'September 7th 2015',
-  'name': 'Giro Di San Francisco'
-}, {
-  'date': 'September 12th 2015',
-  'name': '52nd Mt. Tamalpais Hill Climb'
-}, {
-  'date': 'September 20th 2015',
-  'name': 'Fremont Peak Hill CLimb'
-}, {
-  'date': 'September 20th 2015',
-  'name': 'Oakland Grand Prix'
-}, {
-  'date': 'September 26th 2015',
-  'name': 'Henleyville Road Race'
-}, {
-  'date': 'September 27th 2015',
-  'name': 'Esparto Time Trial'
-}, {
-  'date': 'October 10th 2015',
-  'name': 'Crush Challenge presented by Amgen'
-}, {
-  'date': 'October 11th 2015',
-  'name': 'Dylan'
-  s 2015 Ride '
-} */
-];
 
-var disciplineData = ['Road', 'MTB', 'Track', 'Cyclocross', 'Clinic'];
-var typeData = ['Road Race', 'Criterium', 'Cyclocross', 'Time Trial', 'Cross Country', 'Cross Country Marathon', 'Downhill', 'Dual Slalom', 'Short Track Cross Country', 'Super D', 'Points Race', 'Scratch Race', 'Individual Pursuit', 'Sprint', 'Keirin'];
-var timeData = ['8:00 AM', '8:30 AM', '9:00 AM', '9:20 AM', '10:00 AM', '10:15 AM', '11:40 AM', '12:00 PM', '12:03 PM', '1:00 PM', '1:55 PM', '2:00 PM', '2:18 PM'];
-var promoterData = ['2 Wheel Racing', 'Alta Alpina Cycling Club', 'Alto Velo', 'AntiGravity Cycling', 'Back of Nowhere Enterprises', 'Berkeley Bicycle Club', 'Bicycle Planet', 'Bicycles Plus',
-  'Bike Monkey', 'Bike Station Aptos', 'Brian Joder', 'Bubba Melcher', 'Cal Aggie Cycling', 'Cal Cycling', 'California Giant Cycling Team', 'CCCX (Central Coast Cyclocross)', 'Central Sierra Cyclists',
-  'Chico Corsa Cycling Club', 'China Peak Mountain Resort'
-];
-var streetData = ['Kearny Street', 'Mission Street', 'Polk Street', 'Stockton Street', 'Union Street', 'Third Street', '22nd Street', '49-Mile Scenic Drive', 'Alemany Boulevard', 'Broadway',
-  'Castro Street', 'The Embarcadero', 'Filbert Street', 'Great Highway', 'Haight Street', 'Montgomery Street', 'New Montgomery Street', 'Octavia Boulevard', 'Skyline Boulevard', 'Vermont Street', 'Howard Street'
-];
-var cityData = ['Ross', 'Sacramento', 'St. Helena', 'Salinas', 'San Anselmo', 'San Bernardino', 'San Bruno', 'San Carlos', 'San Clemente', 'San Diego', 'San Dimas', 'San Fernando', 'San Francisco', 'San Gabriel',
-  'San Jacinto', 'San Joaquin', 'San Jose', 'San Juan Bautista', 'San Juan Capistrano', 'San Leandro'
-];
-
-var fullEventData = eventData.map(function(element) {
+var fullEventData = Data.eventData.map(function(event) {
   var newObject = {
     id: 'evt-' + getRandomNumber(10000000, 99999999),
     discipline: getRandomDiscipline(),
     type: getRandomType(),
-    name: element.name,
-    date: element.date,
-    distanceMi: getRandomNumber(1, 200) + ' Mi',
-    durationMin: getRandomNumber(5, 180) + ' Min',
-    elevationGainFt: getRandomNumber(0, 2000) + ' ft',
+    name: event.name,
+    date: event.date,
+    distanceMi: getRandomNumber(1, 200),
+    durationMin: getRandomNumber(5, 180),
+    elevationGainFt: getRandomNumber(0, 2000),
     startTime: getRandomTime(),
     promoter: getRandomPromoter(),
     location: {
@@ -239,40 +22,42 @@ var fullEventData = eventData.map(function(element) {
       state: 'CA',
       zip: '9' + getRandomNumber(1000, 9999)
     },
-    distanceToEventMi: getRandomNumber(1, 200) + ' Mi',
-    estimatedTravelTime: getRandomNumber(1, 5) + ' h',
+    distanceToEventMi: getRandomNumber(1, 200),
+    estimatedTravelTime: getRandomNumber(1, 5) + 'h',
     flyerUrl: 'http://www.ncnca.org/sites/default/files/01/01/2014%20-%202%3A29am/183%20San%20Bruno%20Mountain%20Hill%20Climb%20OKED%203.pdf',
     registrationUrl: 'https://www.usacycling.org/register/2014-183'
   };
   return newObject;
 });
 
-console.log(util.inspect(fullEventData, {colors: true}));
+console.log(util.inspect(fullEventData, {
+  colors: true
+}));
 
 function getRandomNumber(startNumber, endNumber) {
   return Math.floor((Math.random() * endNumber) + startNumber);
 };
 
 function getRandomDiscipline() {
-  return disciplineData[getRandomNumber(0, disciplineData.length)];
+  return Data.disciplineData[getRandomNumber(0, Data.disciplineData.length)];
 };
 
 function getRandomType() {
-  return typeData[getRandomNumber(0, typeData.length)];
+  return Data.typeData[getRandomNumber(0, Data.typeData.length)];
 };
 
 function getRandomTime() {
-  return timeData[getRandomNumber(0, timeData.length)];
+  return Data.timeData[getRandomNumber(0, Data.timeData.length)];
 };
 
 function getRandomPromoter() {
-  return promoterData[getRandomNumber(0, promoterData.length)];
+  return Data.promoterData[getRandomNumber(0, Data.promoterData.length)];
 };
 
 function getRandomStreet() {
-  return streetData[getRandomNumber(0, streetData.length)];
+  return Data.streetData[getRandomNumber(0, Data.streetData.length)];
 };
 
 function getRandomCity() {
-  return cityData[getRandomNumber(0, cityData.length)];
+  return Data.cityData[getRandomNumber(0, Data.cityData.length)];
 }
