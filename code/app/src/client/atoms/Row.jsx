969e87c3349be06704f8e01831e934fb74825e81
Restuta <restuta8@gmail.com>
2015-11-02 19:01:33 -0800

    moved app files to code/app folder so we can have other code grouped nicely under "code"

--- code/app/src/client/atoms/Row.jsx
+++ code/app/src/client/atoms/Row.jsx
@@ -17,6 +17,7 @@ export default class Row extends Component {
 
   //<Col xs={12} md={8} />
   //<Col c="col-xs-12 col-md-8" />
+  //<Col c="xs-12 md-8 push-xs-3"
   //<Col {xs: 12, md: 8} />
-  //
+  //<Col sm="12" md="8" />
 }
