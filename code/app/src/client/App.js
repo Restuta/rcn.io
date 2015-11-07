f6989de322dc2476c66527b8b442a41d68c5bb5b
Restuta <restuta8@gmail.com>
2015-11-07 15:26:52 -0800

    added 16 columns on top to play with margins

--- code/app/src/client/App.js
+++ code/app/src/client/App.js
@@ -3,17 +3,34 @@ import Component from 'react-pure-render/component';
 import './app.scss';
 import Row from './atoms/Row.jsx';
 import Col from './atoms/Col.jsx';
-import Counter from './tmp/Counter.jsx';
-import Event from './tmp/EventStub.jsx';
+import Counter from './temp/Counter.jsx';
+import Event from './temp/EventStub.jsx';
+
+
+function times(x, times) {
+  let arr = [];
+
+  for (let i = 0; i < times; i++) {
+    arr.push(x);
+  }
+
+  return arr;
+}
 
 export class App extends Component {
   render() {
     let genericWeek = [2, 2, 2, 2, 2, 2, 2];
     let roadWeek = [1, 1, 1, 1, 2, 4, 4];
+    let bsColumnsTest = times(1, 16);
 
     return (
       <div>
         <div className="container">
+          <Row style={{marginTop: '20'}}>
+            {bsColumnsTest.map((x, i) =>
+              <Col key={i} sm={x}>{++i}<Event/></Col>)
+            }
+          </Row>
           <Row>
             <Col sm={12} className="transparent">
               <h1 className="oswald">Road Races in CA, 100mi range </h1>
@@ -22,7 +39,7 @@ export class App extends Component {
           <Row>
             <Col className="col-sm-offset-1"/>
             {genericWeek.map((x, i) =>
-              <Col key={i} sm={x}>{++i}<Event/></Col>)
+              <Col key={i} sm={x}>{++i}<Event/> </Col>)
             }
           </Row>
           <Row style={{marginTop: '20'}}>
@@ -31,6 +48,7 @@ export class App extends Component {
               <Col key={i} sm={x}>{++i}<Event/></Col>)
             }
           </Row>
+
         </div>
         <Counter increment={1} color="silver" marginTop="20px" />
       </div>
