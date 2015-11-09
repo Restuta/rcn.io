0c1d9380cfb3f354a1418563da395bf3fe9931e6
Restuta <restuta8@gmail.com>
2015-11-08 20:29:01 -0800

    moved .babelrc config to webpack's config, see https://github.com/gaearon/react-transform-hmr/issues/5 for more details

--- code/app/webpack.config.dev.js
+++ code/app/webpack.config.dev.js
@@ -9,6 +9,8 @@ function resolveNodeModulesPath(pathName) {
 //NOTE: use min versions for prod and to speed-up build times a little
 const pathToReact = resolveNodeModulesPath('react/dist/react.js');
 const pathToReactDOM = resolveNodeModulesPath('react-dom/dist/react-dom.js');
+// const pathToReact = resolveNodeModulesPath('react/dist/react.min.js');
+// const pathToReactDOM = resolveNodeModulesPath('react-dom/dist/react-dom.min.js');
 
 
 module.exports = {
@@ -47,13 +49,30 @@ module.exports = {
     noParse: [pathToReact],
     loaders: [{
       test: /\.jsx?$/,
-      loaders: ['babel?cacheDirectory'],
+      loader: 'babel',
+      query: {
+        'stage': 0,
+        'plugins': ['react-transform'],
+        'cacheDirectory': true, //not needed in for build
+        'extra': {
+          'react-transform': {
+            'transforms': [{
+              'transform': 'react-transform-hmr',
+              'imports': ['react'],
+              'locals': ['module']
+            }, {
+              'transform': 'react-transform-catch-errors',
+              'imports': ['react', 'redbox-react']
+            }]
+          }
+        }
+      },
       exclude: /(node_modules|bower_components)/,
       include: path.join(consts.SRC_DIR, 'client')
     }, {
       test: /\.scss$/,
       loaders: ['style', 'css', 'sass'],
-      exclude: /node_modules/,
+      exclude: /(node_modules|bower_components)/,
       include: path.join(consts.SRC_DIR, 'client')
     }, {
       test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
