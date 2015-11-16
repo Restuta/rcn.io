5bda8250101fab7d6e08be7e347cba0737ac5510
Restuta <restuta8@gmail.com>
2015-11-15 17:50:48 -0800

    setting horizontal base-line like on beta.gridlover.net

--- code/app/src/client/App.js
+++ code/app/src/client/App.js
@@ -20,21 +20,51 @@ function times(x, times) {
 
 export class App extends Component {
   render() {
+    let justGrid = times(1, 16);
     let genericWeek = times(2, 7);
     let roadWeek = [1, 1, 1, 1, 2, 4, 4];
     let fullSpaceWeek = times(2, 5).concat([3, 3]);
 
     const fontBaseSizePx = 14;
 
-    const pStyle = ({fontSize}) => {
-      return {
-        fontSize: fontSize
-      }
-    };
-
     return (
       <div>
         <div className="container">
+          <Row>
+            <Col sm={12}>
+              <h1>ROAD RACES IN CA</h1>
+              <h2>Event #1 The Bump</h2>
+              <p>Bushwick Schlitz. Est Shoreditch small batch, dolor Schlitz sapiente twee stumptown ex. Duis Carles pickled, cornhole Thundercats McSweeney's minim PBR vegan Tumblr irony. Kogi eu Thundercats, sed scenester before they sold out et aesthetic. Elit cred Vice ethical pickled sartorial. Stumptown roof party freegan High Life vero, ea sed minim meggings.</p>
+
+              <h3>New in California in 2016</h3>
+              <p>Cosby sweater plaid shabby chic kitsch pour-over ex. Try-hard fanny pack mumblecore cornhole cray scenester. Assumenda narwhal occupy, Blue Bottle nihil culpa fingerstache. Meggings kogi vinyl meh, food truck banh mi Etsy magna 90's duis typewriter banjo organic leggings Vice.</p>
+
+              <h4>Only 24mi away (45 min drive)</h4>
+              <ul>
+                <li>Roof party put a bird on it incididunt sed umami craft beer cred.</li>
+                <li>Carles literally normcore, Williamsburg Echo Park fingerstache photo booth twee keffiyeh chambray whatever.</li>
+                <li>Scenester High Life Banksy, proident master cleanse tousled squid sriracha ad chillwave post-ironic retro.</li>
+              </ul>
+
+              <h5>Heading 5</h5>
+              <p>Laboris selfies occaecat umami, forage Tumblr American Apparel. Retro Terry Richardson culpa id swag polaroid Intelligentsia American Apparel eu, esse non post-ironic fugiat master cleanse. Direct trade gluten-free blog, fanny pack cray labore skateboard before they sold out adipisicing non magna id Helvetica freegan. Disrupt aliqua Brooklyn church-key lo-fi dreamcatcher.</p>
+
+              <h1>Heading 1</h1>
+              <p>And a paragraph after</p>
+
+              <h2>Heading 1</h2>
+              <p>And a paragraph after</p>
+
+              <h3>Heading 1</h3>
+              <p>And a paragraph after</p>
+            </Col>
+          </Row>
+          <Row style={{marginTop: '20'}}>
+            {justGrid.map((x, i) =>
+              <Col key={i} sm={x} className="col outlined debug">{++i}<Event/></Col>)
+            }
+          </Row>
+
           <Row>
             <Col sm={16}>
               <h1 className="oswald">Road Races in CA, 100mi range</h1>
@@ -63,17 +93,6 @@ export class App extends Component {
               <Col key={i} sm={x} className="col outlined debug">{++i}<Event/></Col>)
             }
           </Row>
-
-          <Row>
-            <Col sm={4}>
-              <h1 >NEW EVENT</h1>
-              <p style={pStyle({fontSize: 14})}>
-                Dunnigan hills road race. Usually held in Mountain view. This is 60mi away from you
-                and looks like a 2h drive without a traffic.
-              </p>
-            </Col>
-          </Row>
-
 
           <Counter increment={1} color="silver" marginTop="20px" />
           <TypographicScale baseSizeInPx={14} scale={1.333}>Perfect Fourth (by David Kadavy), 5 7 9 12 16 21 28 37 50 67 89 111 148</TypographicScale>
