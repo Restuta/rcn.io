93847907ce076c0ef79b089631be3314712f31df
Restuta <restuta8@gmail.com>
2015-11-24 19:51:20 -0800

    added example calculation to EventStub component that accesses the DOM to get actual width of the element and then sets height according to golden ration as a 1/1.618 of the height + added todo what to do with this in future

--- code/app/src/client/temp/EventStub.jsx
+++ code/app/src/client/temp/EventStub.jsx
@@ -3,9 +3,28 @@ import Component from 'react-pure-render/component';
 import './EventStub.scss';
 
 export default class EventStub extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      height: 50
+    };
+  }
+
+  componentDidMount() {
+    //TODO: this is used to get actual size from the browser after component is rendred and results in second react-pure-render
+    //we can avoid this by moving that into top-level HOC that first gets size of the browser window and then renders children
+    //into it passing actual size, so only topmost component would be rendred twice, which is few ms
+    this.setState({height: this.div.offsetWidth / 1.618});
+  }
+
   render() {
+    let style = {
+      height: this.state.height + 'px'
+    };
+
     return (
-      <div className="EventStub"></div>
+      <div style={style} ref={(x) => this.div = x} className="EventStub"></div>
     );
   }
 };
