ff57e23b4f3c453b53da6734c561d96eab2faae3
Restuta <restuta8@gmail.com>
2015-11-25 22:05:27 -0800

    extended Col.jsx with offset properties

--- code/app/src/client/atoms/Col.jsx
+++ code/app/src/client/atoms/Col.jsx
@@ -7,7 +7,11 @@ function getColClassName(propName, propValue) {
     'xs': 'col-xs-',
     'sm': 'col-sm-',
     'md': 'col-md-',
-    'lg': 'col-lg-'
+    'lg': 'col-lg-',
+    'xsOffset': 'col-xs-offset-',
+    'smOffset': 'col-sm-offset-',
+    'mdOffset': 'col-md-offset-',
+    'lgOffset': 'col-lg-offset-',
   };
 
   return validProps[propName] ? validProps[propName] + propValue : '';
@@ -15,12 +19,7 @@ function getColClassName(propName, propValue) {
 
 //usage <Col xs={12} md={8} />
 export default class Col extends Component {
-  static propTypes = {
-    xs: PropTypes.number,
-    sm: PropTypes.number,
-    md: PropTypes.number,
-    lg: PropTypes.number
-  }
+  static propTypes = propTypes
 
   render() {
     //TODO: move this to it's own module
@@ -37,6 +36,17 @@ export default class Col extends Component {
       </div>
     );
   }
+};
+
+const propTypes = {
+  xs: PropTypes.number,
+  sm: PropTypes.number,
+  md: PropTypes.number,
+  lg: PropTypes.number,
+  xsOffset: PropTypes.number,
+  smOffset: PropTypes.number,
+  mdOffset: PropTypes.number,
+  lgOffset: PropTypes.number
 };
 
 //TODO: use functional component when https://github.com/gaearon/babel-plugin-react-transform/pull/34 is mereged
