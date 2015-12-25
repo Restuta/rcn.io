6e9c6d6d8b506ef29ab2da07bcd5b62a180c4df9
Restuta <restuta8@gmail.com>
2015-12-24 18:46:46 -0800

    added all possible 14 col grid week examples

--- code/app/src/client/App.js
+++ code/app/src/client/App.js
@@ -22,7 +22,7 @@ const S10 = () => (<S width={10}/>);
 export class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {appLevelClasses: 'debug-baseline'};
+    this.state = {appLevelClasses: 'debug-baseline debug-container'};
   }
   render() {
     const setAppStateClasses = classesToSet => {
@@ -44,7 +44,7 @@ export class App extends Component {
             </Row>
             <Row className="margin-top">
               <Col sm={2}><h5>Round Badges: </h5></Col>
-              <Col sm={14} className="display-flex">
+              <Col sm={12} className="display-flex">
                 <RoundBadge size={2}>8</RoundBadge>
                 <RoundBadge size={1}>8</RoundBadge>
                 <RoundBadge size={2}><Icon name="car"/></RoundBadge>
@@ -62,7 +62,7 @@ export class App extends Component {
             </Row>
             <Row>
               <Col sm={2}><h5>Not so Round Badges: </h5></Col>
-              <Col sm={14} className="display-flex">
+              <Col sm={12} className="display-flex">
                 <Badge>120mi</Badge><S5/>
                 <Badge>8</Badge><S5/>
                 <Badge><Icon name="trophy"/></Badge><S5/>
@@ -73,7 +73,7 @@ export class App extends Component {
             </Row>
             <Row>
               <Col sm={2}><h5>Square Badges: </h5></Col>
-              <Col sm={14} className="display-flex">
+              <Col sm={12} className="display-flex">
                 <SquareBadge>120mi</SquareBadge><S5/>
                 <SquareBadge>8</SquareBadge><S5/>
                 <SquareBadge><Icon name="trophy"/></SquareBadge><S5/>
@@ -83,7 +83,7 @@ export class App extends Component {
             </Row>
             <Row>
               <Col sm={2}><h5>Square Badges (Inverted): </h5></Col>
-              <Col sm={14} className="display-flex">
+              <Col sm={12} className="display-flex">
                 <SquareBadge className="Inverted">120mi</SquareBadge><S5/>
                 <SquareBadge className="Inverted">8</SquareBadge><S5/>
                 <SquareBadge className="Inverted"><Icon name="trophy"/></SquareBadge><S5/>
@@ -93,7 +93,7 @@ export class App extends Component {
             </Row>
             <Row>
               <Col sm={2}><h5>Elevation:</h5></Col>
-              <Col sm={14} className="display-flex">
+              <Col sm={12} className="display-flex">
                 <Icon name="arrow-up" color={Colors.grey500}/>1200ft<S10/>
                 <Icon name="long-arrow-up" color={Colors.grey500}/>1200ft<S10/>
                 <Badge><Icon name="arrow-up"/>1200ft<S10/></Badge><S5/>
@@ -102,7 +102,7 @@ export class App extends Component {
             </Row>
             <Row>
               <Col sm={2}><h5>Distance:</h5></Col>
-              <Col sm={14} className="display-flex">
+              <Col sm={12} className="display-flex">
                 <Icon name="bicycle" color={Colors.grey500}/>20mi<S10/>
                 <Badge><Icon name="bicycle"/>20mi</Badge><S10/>
                 <Badge className="Inverted"><Icon name="bicycle"/>20mi</Badge><S10/>
@@ -111,7 +111,7 @@ export class App extends Component {
             </Row>
             <Row>
               <Col sm={2}><h5>Duration:</h5></Col>
-              <Col sm={14} className="display-flex">
+              <Col sm={12} className="display-flex">
                 <Icon name="hourglass-start" color={Colors.grey500}/>60min
                 <S10/>
                 <Badge><Icon name="hourglass-start"/>60min</Badge><S5/>
@@ -180,17 +180,16 @@ export class App extends Component {
           <WeekExample days={[1, 1, 1, 1, 2, 5, 5]}/>
           */}
 
-          {/* 14 col examples
           <WeekExample days={[1, 1, 1, 1, 2, 4, 4]}/>
-          <WeekExample days={[1, 1, 1, 1, 4, 4, 4]}/>
-          <WeekExample days={[1, 1, 2, 3, 3, 3, 3]}/>
-          <WeekExample days={[1, 2, 2, 2, 3, 3, 3]}/>
-          <WeekExample days={[2, 2, 2, 2, 2, 3, 3]}/>
+          <WeekExample days={[1, 1, 1, 1, 3, 3, 4]}/>
+          <WeekExample days={[1, 1, 1, 2, 2, 3, 4]}/>
+          <WeekExample days={[1, 1, 2, 2, 2, 2, 4]}/>
+
+          <WeekExample days={[1, 1, 1, 2, 3, 3, 3]}/>
+          <WeekExample days={[1, 1, 2, 2, 2, 3, 3]}/>
+          <WeekExample days={[1, 2, 2, 2, 2, 2, 3]}/>
           <WeekExample days={[2, 2, 2, 2, 2, 2, 2]} allSameSize/>
-          <WeekExample days={[2, 2, 2, 2, 2, 3, 3]}/>
-          <WeekExample days={[1, 1, 1, 1, 3, 4, 4]}/>
-          <WeekExample days={[1, 1, 1, 1, 2, 5, 5]}/>
-          */}
+          <WeekExample days={[2, 2, 2, 2, 2, 2, 2]}/>
 
 
           <Counter increment={1} color="silver" marginTop="20px" />
