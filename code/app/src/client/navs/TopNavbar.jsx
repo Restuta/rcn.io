3b9f6cd0f22b64053760fd23615ff47cbeb8fdc7
Restuta <restuta8@gmail.com>
2016-02-15 13:37:26 -0800

    bye-bye floats

--- code/app/src/client/navs/TopNavbar.jsx
+++ code/app/src/client/navs/TopNavbar.jsx
@@ -2,29 +2,30 @@ import React from 'react'
 import Component from 'react-pure-render/component'
 import classnames from 'classnames'
 import './TopNavbar.scss'
-import { Link } from 'react-router'
+import {Link} from 'react-router'
 
 export default class TopNavbar extends Component {
   render() {
     const classNames = classnames('TopNavbar', 'navbar  bg-faded')
-
     return (
-
       <nav className={classNames}>
         <div className="content">
-           <a className="navbar-brand" href="#">RCN</a>
-           <div className="navbar-brand-description">Plan your season</div>
-           <ul className="nav navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link" to={'/cal'}>Cal</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link faded" to={'/dev'}>Dev</Link>
-            </li>
-           </ul>
+          <div className="content-left">
+            <a className="navbar-brand" href="#">RCN</a>
+            <div className="navbar-brand-description">Plan your season</div>
+          </div>
+          <div className="content-right">
+            <ul className="nav navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" to={'/cal'}>Cal</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link faded" to={'/dev'}>Dev</Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
-
     )
   }
 }
