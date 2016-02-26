af2c8bd0a2a01e34f7a147cb2b8d7e87e19b0e25
Restuta <restuta8@gmail.com>
2016-02-25 22:55:18 -0800

    upgrading ESlint to v2

---
+++ code/app/src/client/calendar/WeekdaysHeader.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Component from 'react-pure-render/component'
+
+export default class WeekdaysHeader extends Component {
+  render() {
+    return (
+      <div className="WeekDaysHeader"></div>
+    )
+  }
+}
