5375a969e990adb3c16a2228b045d7191a4d254d
Helen Akimova <me4ta18@gmail.com>
2016-03-26 17:06:30 -0700

    created generator for MTB races with known data

---
+++ code/data/fake-events-generator/original/2016-mtb-generator.js
@@ -0,0 +1,28 @@
+import util from 'util';
+import Data from './2016-mtb';
+
+
+var fullEventData = Data.eventData.map(function(event) {
+  var newObject = {
+    id: 'evt-' + getRandomNumber(10000000, 99999999),
+    discipline: 'MTB',
+    name: event.name,
+    date: event.date,
+    promoterUrl: event.promoterUrl,
+    location: {
+      city: event.locationCity,
+      state: 'CA'
+    }
+  };
+  return newObject;
+});
+
+// console.log(util.inspect(fullEventData, {
+//   colors: true
+// }));
+
+console.log(JSON.stringify(fullEventData, null, '  '));
+
+function getRandomNumber(startNumber, endNumber) {
+  return Math.floor((Math.random() * endNumber) + startNumber);
+}
