aa1d70f4642c01988537fa55fb8ed650362e4d85
Restuta <restuta8@gmail.com>
2016-04-09 15:59:05 -0700

    added gzip compression and SS logging for dev and prod (using morgan for now)

--- code/app/server.js
+++ code/app/server.js
@@ -1,11 +1,23 @@
 const path = require('path')
 const express = require('express')
+const compression = require('compression')
+const morgan = require('morgan')
 const app = express()
-
 // const React = require('react')
 // const Router = require('react-router')
 
+const EnvIsProd = process.env.NODE_ENV === 'production'
+const Config = {
+  morganLogType: EnvIsProd ? 'combined' : 'dev',
+  port: EnvIsProd ? 80 : 3888,
+}
+
+app.use(morgan(Config.morganLogType))
 app.use(express.static(path.join(__dirname, '/dist')))
+
+if (EnvIsProd) {
+  app.use(compression())
+}
 
 app.get('/*', function(req, res, next) {
   res.sendFile(path.join(__dirname, '/dist/index.html'))
@@ -17,7 +29,7 @@ app.get('/*', function(req, res, next) {
 })
 
 
-const server = app.listen(3888, function() {
+const server = app.listen(Config.port, function() {
   const host = server.address().address === '::' ? 'localhost' : server.address().address
   const port = server.address().port
 
