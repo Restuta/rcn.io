33877dc71a38c782726218c241fa779560c26370
Restuta <restuta8@gmail.com>
2016-04-26 22:18:50 -0700

    adding polyfill for IE11 for string.startsWith()

--- src/client/utils/polyfills.js
+++ src/client/utils/polyfills.js
@@ -24,3 +24,12 @@ if (typeof Object.assign != 'function') {
     };
   })();
 }
+
+
+//string.startsWith() for IE
+if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function(searchString, position){
+      position = position || 0;
+      return this.substr(position, searchString.length) === searchString;
+  };
+}
