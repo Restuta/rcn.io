997ef50a8a2a6258ca2ecf9eb73305b0ad949e96
Restuta <restuta8@gmail.com>
2016-05-03 13:44:34 -0700

    updated logo and home page

--- src/client/navs/Logo.jsx
+++ src/client/navs/Logo.jsx
@@ -1,20 +1,14 @@
 import React from 'react'
 import Component from 'react-pure-render/component'
 import InlineSVG from 'svg-inline-react'
-import LogoSVG from './logo.svg'
+import LogoSVG from 'navs/logo.svg'
+import './Logo.scss'
 
 
 export default class Logo extends Component {
   render() {
-
-    const style = {
-      width: '2.5rem',
-      height: '4rem',
-      marginBottom: -2 //should go away when we use logo with no paddings
-    }
-
     return (
-      <InlineSVG  style={style} src={LogoSVG} />
+      <InlineSVG src={LogoSVG} className="Logo" />
     )
   }
 }
