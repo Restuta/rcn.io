605342c025d2cb3f97b9db4b7542a2fe096d3fc8
Restuta <restuta8@gmail.com>
2016-06-10 23:59:31 -0700

    adding react-modal2 (thanks @thejameskyle for a great name!) since it's really the only react modal that is exactly what I need, minimal, un-opinionated, universal

--- src/client/calendar/events/EventDetails.jsx
+++ src/client/calendar/events/EventDetails.jsx
@@ -1,32 +1,34 @@
 import React, {PropTypes} from 'react'
 import Component from 'react-pure-render/component'
 import './EventDetails.scss'
-
-import Modal from 'react-modal'
+import ExecutionEnvironment from 'exenv'
+import Row from 'atoms/Row.jsx'
+import Col from 'atoms/Col.jsx'
+import { default as ReactModal } from 'react-modal2'
 
 class MyModal extends Component {
   componentWillMount() {
-    document.body.style.overflow = 'hidden'
+    if (ExecutionEnvironment.canUseDOM) {
+      document.body.style.overflow = 'hidden'
+    }
   }
 
   componentWillUnmount() {
-    document.body.style.overflow = null
+    if (ExecutionEnvironment.canUseDOM) {
+      document.body.style.overflow = null
+    }
   }
 
   render() {
     return (
-      <div className="MyModal overlay">
-        <div className="content">
-          <div>
-            <h4 style={{width: '70%', marginTop: 0, color: 'grey'}} className="header-regular w-500">
-              Saturday, May 13th <i>(in 43 days)</i>
-            </h4>
-            <h3 style={{width: '65%', marginTop: 0}} className="header-regular w-900">Chico Stage Race pb Sierra Nevada Brewing
-              Co — Stage 4: Steve Harrison Memorial Criterium
-            </h3>
-          </div>
-        </div>
-      </div>
+      <ReactModal
+        onClose={this.props.onClose}
+        closeOnEsc={this.props.closeOnEsc}
+        closeOnBackdropClick={this.props.closeOnBackdropClick}
+        backdropClassName='MyModal overlay'
+        modalClassName='content'>
+        {this.props.children}
+      </ReactModal>
     )
   }
 }
@@ -55,23 +57,21 @@ export default class EventDetails extends Component {
         Event Details of: {eventId}
         <hr />
         <a onClick={this.openModal}>Open Modal</a>
-        <MyModal />
-        <Modal
-          isOpen={this.state.modalIsOpen}
-          onAfterOpen={this.afterOpenModal}
-          onRequestClose={this.closeModal}
-          >
+        {this.state.modalIsOpen &&
+          <MyModal closeOnEsc={true} closeOnBackdropClick={true} onClose={this.closeModal}>
+            <Row>
+              <Col sm={9}>
+                <h4 style={{marginTop: 0, color: 'grey'}} className="header-regular w-500">
+                  Saturday, May 13th <i>(in 43 days)</i>
+                </h4>
+                <h3 style={{marginTop: 0}} className="header-regular w-900">Chico Stage Race pb Sierra Nevada Brewing
+                  Co — Stage 4: Steve Harrison Memorial Criterium
+                </h3>
+              </Col>
+            </Row>
+          </MyModal>
+        }
 
-          <button onClick={this.closeModal}>close</button>
-          <div>I am a modal</div>
-          <form>
-            <input />
-            <button>tab navigation</button>
-            <button>stays</button>
-            <button>inside</button>
-            <button>the modal</button>
-          </form>
-        </Modal>
       </div>
     )
   }
