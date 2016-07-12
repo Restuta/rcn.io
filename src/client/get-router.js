ce266ca0c92ecd207c89ba970f23ed017a9f7f08
Restuta <restuta8@gmail.com>
2016-07-11 23:13:13 -0700

    enabled back analytics

--- src/client/get-router.js
+++ src/client/get-router.js
@@ -7,7 +7,7 @@ import routes from 'routes'
 
 const store = configureStore()
 const history = syncHistoryWithStore(browserHistory, store)
-// history.listen(location => analytics.page())
+history.listen(location => analytics.page())
 
 //overriding Router function to pass custom props to a child components, building a higer order function to
 //provide containerWidth to inner-clojure
