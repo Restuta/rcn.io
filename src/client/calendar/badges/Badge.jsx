6b6764c1cb6d478c57733b61c9b5ccdad8d8da88
Restuta <restuta8@gmail.com>
2016-08-15 22:59:50 -0700

    fixed incorrect prop type

--- src/client/calendar/badges/Badge.jsx
+++ src/client/calendar/badges/Badge.jsx
@@ -55,5 +55,5 @@ Badge.propTypes = {
   bgColor: PropTypes.string,
   heightRem: PropTypes.oneOf([2, 3, 4, 5, 6, 7, 8]),
   className: PropTypes.string,
-  customFontSize: PropTypes.boo,
+  customFontSize: PropTypes.bool,
 }
