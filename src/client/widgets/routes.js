632c5f79a39155a7a52ac8dbde2816d66b72f643
Restuta <restuta8@gmail.com>
2016-10-14 22:47:53 -0700

    removing test code from routes.js

--- src/client/widgets/routes.js
+++ src/client/widgets/routes.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import { Route } from 'react-router'
 import WidgetRoot from './WidgetRoot'
-
-
-const Test = (props) => (<div>🦁 Ok, come here, bunny.</div>)
+import UpcomingEvents from 'widgets/events/UpcomingEvents.jsx'
 
 //alternative way is to define route root as /widgets and remove root in get-routes.js
 const routes  = (
   <Route path="/" component={WidgetRoot}>
     {/* <IndexRoute component={Index} /> */}
-    <Route path="bla" component={Test}/>
+    <Route path="upcoming-events" component={UpcomingEvents}/>
     {/* <Route path="*" component={Home}/> */}
   </Route>
 )
