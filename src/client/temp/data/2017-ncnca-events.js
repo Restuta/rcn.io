68c9273b8c506290a7db6e2d2a841d89ec51fb00
Helen Vynogradenko <me4ta18@gmail.com>
2016-12-01 14:04:48 -0800

    add Cyclo-cross National Championships from 3rd till 8th

--- src/client/temp/data/2017-ncnca-events.js
+++ src/client/temp/data/2017-ncnca-events.js
@@ -18,7 +18,128 @@ export default [{
       }
     ],
     "group": "1"
+  }, /*{
+    "id": "evt-ncnca-2017-jan-03-cx-gkV",
+    "name": "USA Cycling Cyclo-cross National Championships",
+    "date": "January 03 2017",
+    "discipline": "Cyclocross",
+    "type": "Cyclocross",
+    "location": {
+      "city": "Hartford",
+      "state": "CT"
+    },
+    "promoters": [
+      {
+        "name": "",
+        "contactName": "",
+        "contactInfo": "http://www.ncnca.org/ncncapromoter/usa-cycling"
+      }
+    ],
+    "websiteUrl" : "http://www.usacycling.org/2017/cxnationals",
+    "series" : "http://www.ncnca.org/ncncaseries/2017-usac-national-championships",
+    "group": "1"
   }, {
+    "id": "evt-ncnca-2017-jan-04-cx-gkV",
+    "name": "USA Cycling Cyclo-cross National Championships",
+    "date": "January 04 2017",
+    "discipline": "Cyclocross",
+    "type": "Cyclocross",
+    "location": {
+      "city": "Hartford",
+      "state": "CT"
+    },
+    "promoters": [
+      {
+        "name": "",
+        "contactName": "",
+        "contactInfo": "http://www.ncnca.org/ncncapromoter/usa-cycling"
+      }
+    ],
+    "websiteUrl" : "http://www.usacycling.org/2017/cxnationals",
+    "series" : "http://www.ncnca.org/ncncaseries/2017-usac-national-championships",
+    "group": "1"
+  }, {
+    "id": "evt-ncnca-2017-jan-05-cx-gkV",
+    "name": "USA Cycling Cyclo-cross National Championships",
+    "date": "January 05 2017",
+    "discipline": "Cyclocross",
+    "type": "Cyclocross",
+    "location": {
+      "city": "Hartford",
+      "state": "CT"
+    },
+    "promoters": [
+      {
+        "name": "",
+        "contactName": "",
+        "contactInfo": "http://www.ncnca.org/ncncapromoter/usa-cycling"
+      }
+    ],
+    "websiteUrl" : "http://www.usacycling.org/2017/cxnationals",
+    "series" : "http://www.ncnca.org/ncncaseries/2017-usac-national-championships",
+    "group": "1"
+  }, {
+    "id": "evt-ncnca-2017-jan-06-cx-gkV",
+    "name": "USA Cycling Cyclo-cross National Championships",
+    "date": "January 06 2017",
+    "discipline": "Cyclocross",
+    "type": "Cyclocross",
+    "location": {
+      "city": "Hartford",
+      "state": "CT"
+    },
+    "promoters": [
+      {
+        "name": "",
+        "contactName": "",
+        "contactInfo": "http://www.ncnca.org/ncncapromoter/usa-cycling"
+      }
+    ],
+    "websiteUrl" : "http://www.usacycling.org/2017/cxnationals",
+    "series" : "http://www.ncnca.org/ncncaseries/2017-usac-national-championships",
+    "group": "1"
+  }, {
+    "id": "evt-ncnca-2017-jan-07-cx-gkV",
+    "name": "USA Cycling Cyclo-cross National Championships",
+    "date": "January 07 2017",
+    "discipline": "Cyclocross",
+    "type": "Cyclocross",
+    "location": {
+      "city": "Hartford",
+      "state": "CT"
+    },
+    "promoters": [
+      {
+        "name": "",
+        "contactName": "",
+        "contactInfo": "http://www.ncnca.org/ncncapromoter/usa-cycling"
+      }
+    ],
+    "websiteUrl" : "http://www.usacycling.org/2017/cxnationals",
+    "series" : "http://www.ncnca.org/ncncaseries/2017-usac-national-championships",
+    "group": "1"
+  }, {
+    "id": "evt-ncnca-2017-jan-08-cx-gkV",
+    "name": "USA Cycling Cyclo-cross National Championships",
+    "date": "January 08 2017",
+    "discipline": "Cyclocross",
+    "type": "Cyclocross",
+    "location": {
+      "city": "Hartford",
+      "state": "CT"
+    },
+    "promoters": [
+      {
+        "name": "",
+        "contactName": "",
+        "contactInfo": "http://www.ncnca.org/ncncapromoter/usa-cycling"
+      }
+    ],
+    "websiteUrl" : "http://www.usacycling.org/2017/cxnationals",
+    "series" : "http://www.ncnca.org/ncncaseries/2017-usac-national-championships",
+    "group": "1"
+  },
+  */ {
     "id": "evt-ncnca-2017-jan-08-ct-sVK",
     "name": "Early Bird Training Criterium",
     "date": "January 08 2017",
