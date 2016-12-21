2cc3c7dfc53769c75870718716d79e6808701538
Restuta <restuta8@gmail.com>
2016-12-20 19:26:51 -0800

    added 2017-ncnca-serieses file with an example series

---
+++ src/client/temp/data/2017-ncnca-serieses.js
@@ -0,0 +1,6 @@
+/*eslint-disable */
+//last id 40001
+
+export default [
+  { "id": "ser-40001", "name": "2017 USAC National Championships", "url": "http://www.ncnca.org/ncncaseries/2017-usac-national-championships" }
+]
