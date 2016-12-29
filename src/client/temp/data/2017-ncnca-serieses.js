62effecd177990bb425c4e77c5dfc47e5a5efce7
Helen Vynogradenko <me4ta18@gmail.com>
2016-12-28 23:47:00 -0800

    add short name field (now we are going to create it for all series)

--- src/client/temp/data/2017-ncnca-serieses.js
+++ src/client/temp/data/2017-ncnca-serieses.js
@@ -2,5 +2,5 @@
 //last id 40001
 
 export default [
-  { "id": "ser-40001", "name": "2017 USAC National Championships", "url": "http://www.ncnca.org/ncncaseries/2017-usac-national-championships" },
+  { "id": "ser-40001", "name": "2017 USAC National Championships", "url": "http://www.ncnca.org/ncncaseries/2017-usac-national-championships", "shortName": "UNC" },
 ]
