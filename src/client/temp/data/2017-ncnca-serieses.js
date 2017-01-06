36428e304dd0bde0d21f2215d99db90479349a3e
Helen Vynogradenko <me4ta18@gmail.com>
2017-01-06 13:45:25 -0800

    add CC and VPRS series

--- src/client/temp/data/2017-ncnca-serieses.js
+++ src/client/temp/data/2017-ncnca-serieses.js
@@ -3,4 +3,6 @@
 
 export default [
   { "id": "ser-40001", "name": "2017 USAC National Championships", "url": "http://www.ncnca.org/ncncaseries/2017-usac-national-championships", "shortName": "UNC" },
+  { "id": "ser-40002", "name": "2017 Cal Cup", "url": "http://www.ncnca.org/ncncaseries/2017-cal-cup", "shortName": "CC" },
+  { "id": "ser-40003", "name": "2017 Velo Promo Road Series", "url": "http://www.ncnca.org/ncncaseries/2017-velo-promo-road-series", "shortName": "VPRS" },
 ]
