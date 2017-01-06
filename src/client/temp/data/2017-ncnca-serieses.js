2ffb15177d0508c8ced0c66ce3db0411b824e206
Helen Vynogradenko <me4ta18@gmail.com>
2017-01-06 13:54:23 -0800

    add 6 more series

--- src/client/temp/data/2017-ncnca-serieses.js
+++ src/client/temp/data/2017-ncnca-serieses.js
@@ -5,4 +5,11 @@ export default [
   { "id": "ser-40001", "name": "2017 USAC National Championships", "url": "http://www.ncnca.org/ncncaseries/2017-usac-national-championships", "shortName": "UNC" },
   { "id": "ser-40002", "name": "2017 Cal Cup", "url": "http://www.ncnca.org/ncncaseries/2017-cal-cup", "shortName": "CC" },
   { "id": "ser-40003", "name": "2017 Velo Promo Road Series", "url": "http://www.ncnca.org/ncncaseries/2017-velo-promo-road-series", "shortName": "VPRS" },
+  { "id": "ser-40004", "name": "2017 Central Coast Road Series", "url": "http://www.ncnca.org/ncncaseries/2017-central-coast-road-series", "shortName": "CCRS" },
+  { "id": "ser-40005", "name": "2017 Clovis Criterium Training Series", "url": "http://www.ncnca.org/ncncaseries/2017-clovis-criterium-training-series", "shortName": "CCTS" },
+  { "id": "ser-40006", "name": "2017 Early Bird Training Series", "url": "http://www.ncnca.org/ncncaseries/2017-early-bird-training-series", "shortName": "EBTS" },
+  { "id": "ser-40007", "name": "2017 Red Kite Omnium", "url": "http://www.ncnca.org/ncncaseries/2017-red-kite-omnium", "shortName": "RKO" },
+  { "id": "ser-40008", "name": "2017 NCNCA Championships", "url": "http://www.ncnca.org/ncncaseries/2017-ncnca-championships", "shortName": "NC" },
+  { "id": "ser-40009", "name": "2017 Western Collegiate Cycling Conference", "url": "http://www.ncnca.org/ncncaseries/2017-western-collegiate-cycling-conference-wccc", "shortName": "WCCC" },
+
 ]
