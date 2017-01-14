312362049d4f6c1e3d4d1dd1cdbc05e26632037a
Restuta <restuta8@gmail.com>
2017-01-13 21:53:45 -0800

    major refactoring in Calendar component + bugfix

--- src/client/calendar/Calendar.jsx
+++ src/client/calendar/Calendar.jsx
@@ -15,6 +15,46 @@ const getEventByDate = (eventsMap, date) => {
   return eventsMap.get(key) || []
 }
 
+const createEventComponents = (events, draft, daySize, containerWidth) => {
+  let eventComponents
+
+  if (events.length > 0) {
+    eventComponents = events.map((event, i) =>
+      <Event id={event.id} key={event.id}
+        widthColumns={daySize}
+        containerWidth={containerWidth}
+        event={event}
+        draft={draft}
+        highlightEventTypeInName
+      />
+    )
+  }
+
+  return eventComponents
+}
+
+const createDayComponent = (key, currentDate, today, daySize, eventComponents, containerWidth) => {
+  const itIsFirstDayOfMonth = firstDayOfMonth(currentDate)
+  const currentDayIsToday = (today.isSame(currentDate, 'days'))
+  const month = currentDate.month() + 1
+  //using to alternate between months so we they become easier to spot in a caledar
+  const currentDayBelongsToAlternateMonth = (currentDate.month() % 2 === 0)
+
+  return (
+    <Day key={key} year={currentDate.year()} month={month} day={currentDate.date()}
+      size={daySize}
+      itIsToday={currentDayIsToday}
+      itIsFirstDayOfMonth={itIsFirstDayOfMonth}
+      itIsLastDayOfMonth={lastDayOfMonth(currentDate)}
+      itIsAlternateMonthsDay={currentDayBelongsToAlternateMonth}
+      containerWidth={containerWidth}
+      dayOfWeek={currentDate.isoWeekday()}
+      weekNumber={currentDate.isoWeek()}>
+      {eventComponents}
+    </Day>
+  )
+}
+
 class Calendar extends Component {
   // constructor(props) {
   //   super(props)
@@ -58,83 +98,53 @@ class Calendar extends Component {
     }
 
     const today = momentTZ()
-    const eventsTotalFromToday = events.getTotalFrom(today)
-    const eventsTotal = events.total
-    const firstDayOfYear = momentTZ({year: year, month: 0, day: 1}).startOf('isoWeek')
 
-    let startDate = firstDayOfYear
-    let totalWeeks = 53
+    const firstDayOfTheYear = momentTZ({year: year, month: 0, day: 1})
+    let calendarStartDate = momentTZ({year: year, month: 0, day: 1}).startOf('isoWeek')
+    let totalWeeksToShow = 53
     //set a date to two weeks back monday, -6 means Monday
-    const twoWeeksBackDay = today.clone().isoWeekday(-6)
-    const twoWeeksBackIsWithinCurrentYear = (twoWeeksBackDay.year() === today.year())
+    const twoMondaysBackDay = today.clone().isoWeekday(-6)
+    const twoWeeksBackIsWithinCurrentYear = (twoMondaysBackDay.year() === today.year())
 
-    // if first day of year is before today only then we wan to show hide/show past link
-    if (firstDayOfYear.isBefore(today) && twoWeeksBackIsWithinCurrentYear && !showPastEvents) {
-      //if event from last week is in current year, then we change total weeks to hide past events
-      startDate = twoWeeksBackDay
-      totalWeeks = totalWeeks - startDate.get('isoWeek')
+    // if first day of year is before today only then we want to show "hide/show past" link
+    if (firstDayOfTheYear.isBefore(today)
+      && twoWeeksBackIsWithinCurrentYear
+      && !showPastEvents
+      && year === today.year()) {
+      calendarStartDate = twoMondaysBackDay
+      //if two weeks back it's still current year, then we change total weeks to hide past events
+      totalWeeksToShow = totalWeeksToShow - calendarStartDate.get('isoWeek')
     }
 
-    if (year === today.year()) {
+    if (year === today.year() && twoWeeksBackIsWithinCurrentYear) {
       shouldShowHidePastLink = true
     }
 
-    let currentDate = startDate.clone()
+    let currentDate = calendarStartDate.clone()
     let weeksComponents = []
 
-    for (let i = 1; i <= totalWeeks; i++) {
+    for (let i = 1; i <= totalWeeksToShow; i++) {
       let daysComponents = []
-
       let weekContainsFirstDayOfMonth = false
 
       for (let k = 1; k <= 7; k++) {
         const daySize = weekdaysSizes[currentDate.isoWeekday() - 1]
-        const month = currentDate.month() + 1
-        const currentDayIsToday = (today.isSame(currentDate, 'days'))
-        //using to alternate between months so we they become easier to spot in a caledar
-        const currentDayBelongsToAlternateMonth = (currentDate.month() % 2 === 0)
         const foundEvents = getEventByDate(events.map, currentDate)
 
-        let eventComponents
+        const eventComponents = createEventComponents(foundEvents, draft, daySize, containerWidth)
+        daysComponents.push(createDayComponent(k, currentDate, today, daySize, eventComponents, containerWidth))
 
-        if (foundEvents.length > 0) {
-          eventComponents = foundEvents.map((event, i) =>
-            <Event id={event.id} key={event.id}
-              widthColumns={daySize}
-              containerWidth={containerWidth}
-              event={event}
-              draft={draft}
-              highlightEventTypeInName
-            />
-          )
-        }
-
-        const itIsFirstDayOfMonth = firstDayOfMonth(currentDate)
-
-        if (itIsFirstDayOfMonth) {
+        if (firstDayOfMonth(currentDate)) {
           weekContainsFirstDayOfMonth = true
         }
 
-        daysComponents.push(
-          <Day key={k} year={currentDate.year()} month={month} day={currentDate.date()}
-            size={daySize}
-            itIsToday={currentDayIsToday}
-            itIsFirstDayOfMonth={itIsFirstDayOfMonth}
-            itIsLastDayOfMonth={lastDayOfMonth(currentDate)}
-            itIsAlternateMonthsDay={currentDayBelongsToAlternateMonth}
-            containerWidth={containerWidth}
-            dayOfWeek={currentDate.isoWeekday()}
-            weekNumber={currentDate.isoWeek()}>
-            {eventComponents}
-          </Day>
-        )
         currentDate.add(1, 'day')
       }
 
       const currentMonth = currentDate.month() + 1
 
       weeksComponents.push(
-        <Week key={i} month={currentMonth} lastOne={i === totalWeeks}
+        <Week key={i} month={currentMonth} lastOne={i === totalWeeksToShow}
           containsFirstDayOfMonth={weekContainsFirstDayOfMonth}>
           {daysComponents}
         </Week>
@@ -142,11 +152,12 @@ class Calendar extends Component {
     }
 
     let subTitleComp
+    const eventsTotalFromToday = events.getTotalFrom(today)
 
     if (showPastEvents || eventsTotalFromToday === 0) {
       subTitleComp = (
         <h3 className="sub-title">
-          {eventsTotal} events
+          {events.total} events
           {shouldShowHidePastLink &&
             <a className="show-more-or-less" onClick={onShowFullHidePastClick}>
               hide past events
@@ -160,7 +171,7 @@ class Calendar extends Component {
           {eventsTotalFromToday} upcoming events from <span className="today-date">Today ({today.format('MMMM Do')})</span>
           {shouldShowHidePastLink
             && (<a className="show-more-or-less" onClick={onShowFullHidePastClick}>
-              show all {eventsTotal} events
+              show all {events.total} events
             </a>)
           }
         </h3>
