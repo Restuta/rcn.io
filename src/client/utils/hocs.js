91a991074150a2cab70f79483332b72dd2d6768c
restuta <restuta8@gmail.com>
2017-05-27 21:56:24 -0700

    made perf hoc to be conditional and only be used for dev

--- src/client/utils/hocs.js
+++ src/client/utils/hocs.js
@@ -1,29 +1,37 @@
 import React from 'react'
 
-// logs component's rendering time in MS
-const logRenderPerf = function(WrappedComp, name = '<Unknown Component Name>') {
-  return class RenderPerf extends React.Component {
-    constructor(props) {
-      super(props)
-      this.whenRenderStarted = 0
-    }
+let logRenderPerf
 
-    componentDidMount() {
-      const now = +new Date()
-      console.log(`  ${name} rendered in: ` + (now - this.whenRenderStarted) + 'ms') // eslint-disable-line  no-console
-    }
 
-    componentDidUpdate() {
-      const now = +new Date()
-      console.log(`  ${name} re-rendered in: ` + (now - this.whenRenderStarted) + 'ms') // eslint-disable-line  no-console
-    }
+if (process.env.NODE_ENV === 'development') {
+  // logs component's rendering time in MS
+  logRenderPerf = function(WrappedComp, name = '<Unknown Component Name>') {
+    return class RenderPerf extends React.Component {
+      constructor(props) {
+        super(props)
+        this.whenRenderStarted = 0
+      }
 
-    render() {
-      this.whenRenderStarted = +new Date()
-      return <WrappedComp {...this.props}/>
+      componentDidMount() {
+        const now = +new Date()
+        console.log(`  ${name} rendered in: ` + (now - this.whenRenderStarted) + 'ms') // eslint-disable-line  no-console
+      }
+
+      componentDidUpdate() {
+        const now = +new Date()
+        console.log(`  ${name} re-rendered in: ` + (now - this.whenRenderStarted) + 'ms') // eslint-disable-line  no-console
+      }
+
+      render() {
+        this.whenRenderStarted = +new Date()
+        return <WrappedComp {...this.props}/>
+      }
     }
   }
+} else {
+  logRenderPerf = WrappedComp => WrappedComp
 }
+
 
 
 export {
