412fb2e22a7157c0672152f4f92ef08dbb2ca7e3
Restuta <restuta8@gmail.com>
2017-05-29 19:56:36 -0700

    added support of “border: true|false” prop

--- src/client/atoms/Alert.jsx
+++ src/client/atoms/Alert.jsx
@@ -2,10 +2,16 @@ import React, { PropTypes } from 'react'
 import Component from 'react-pure-render/component'
 import Icon from 'atoms/Icon.jsx'
 import './Alert.scss'
+import classnames from 'classnames'
 
 export default class Alert extends Component {
   render() {
-    const { showIcon = true, type = 'info', style } = this.props
+    const {
+      showIcon = true,
+      type = 'info',
+      style,
+      border = true,
+    } = this.props
 
     const iconMap = {
       'success': 'check',
@@ -15,8 +21,13 @@ export default class Alert extends Component {
     }
     const iconToShow = iconMap[type]
 
+    console.info(border)
+    const classNames = classnames(`alert alert-${type} Alert`, {
+      'no-border': !border
+    })
+
     return (
-      <div className={`alert alert-${type} Alert`} role="alert" style={style}>
+      <div className={classNames} role="alert" style={style}>
         {showIcon && <Icon name={iconToShow} size={3} style={{marginRight: '0.5rem'}}/>}
         {this.props.children}
       </div>
@@ -28,4 +39,5 @@ Alert.propTypes = {
   showIcon: PropTypes.bool,
   type: PropTypes.oneOf(['success', 'info', 'warning', 'danger']),
   style: PropTypes.object,
+  border: PropTypes.bool
 }
