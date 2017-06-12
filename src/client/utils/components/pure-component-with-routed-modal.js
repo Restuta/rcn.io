e9a9810d88285266efcd364ea5001e3e1a4d53d3
restuta <restuta8@gmail.com>
2017-06-11 22:56:09 -0700

    added a hacky workaround that made all cases work

--- src/client/utils/components/pure-component-with-routed-modal.js
+++ src/client/utils/components/pure-component-with-routed-modal.js
@@ -1,22 +1,22 @@
 import shallowEqual from 'client/utils/shallow-equal-except'
+import { get } from 'lodash'
 
-// will ignore router-specific props that usually cause unwanted re-render when modal is opened and custom
-// route is used for modal aka "pinterest" or "product hunt" style modals
+// TODO BC: back / forward in calendar doesn't work after opening few modals
+// because navigated back from modal stays true
+
+// will ignore router-specific props that usually cause unwanted re-render when modal is opened
+// and custom route is used for modal aka "pinterest" or "product hunt" style modals
 const pureComponentWithRoutedModal = component => {
   // can't be an arrow finction since need bounded thist to component's instance
   component.prototype.shouldComponentUpdate = function(nextProps, nextState) {
     // if navigated back from modal we should not re-render if only routing-related props changed
-    if (
-      nextProps.location &&
-      nextProps.location.state &&
-      (nextProps.location.state.modal || nextProps.location.state.navigatedBackFromModal)
+    if (get(nextProps, 'location.state.modalProps.isOpen')
+      || get(nextProps, 'navigatedBackFromModal')
+      // POP would mean it's browser back, in that case we may want to just do normal shallow compare
+      && get(nextProps, 'location.action') !== 'POP'
     ) {
-      return !shallowEqual(
-        this.props,
-        nextProps,
-        { exceptProps: ['location', 'params', 'routes', 'router'], },
-        component.displayName
-      )
+      // when it's "back from modal" we shoul dignore all the props and should not re-render
+      return false
     }
 
     return !shallowEqual(this.props, nextProps, undefined, component.displayName)
