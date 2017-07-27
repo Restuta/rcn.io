7c2506b9dcd47b821e4904a45c1498099f31a622
Restuta <restuta8@gmail.com>
2017-07-27 07:47:40 -0700

    fixed 2016 events to confirm to schema and fixed schema to be more forgiving

--- src/client/temp/data/tests/event-schema.js
+++ src/client/temp/data/tests/event-schema.js
@@ -13,41 +13,41 @@ const getAllEventTypes = eventTypesMap =>
 const schema = Joi.object().keys({
   // TODO: define id regex
   id: Joi.string().trim().required(),
-  _shortId: Joi.string().regex(/[a-zA-Z0-9_$]+$/gm).trim().required(),
-  name: Joi.string().min(10).trim().required(),
+  _shortId: Joi.string().regex(/[a-zA-Z0-9_$]+$/gm).trim(),
+  name: Joi.string().min(5).trim().required(),
   // TODO: define custom validation
   date: Joi.string().trim().required(),
   type: Joi.any().valid(getAllEventTypes(EventTypes)),
   discipline: Joi.any().valid(values(Disciplines)),
-  usacPermit: Joi.string().regex(/(19|20)\d\d-\d{1,5}/),
+  usacPermit: Joi.string().regex(/(19|20)\d\d-\d{1,5}/).allow(''),
   location: Joi.object().keys({
-    name: Joi.string().trim().min(5),
+    name: Joi.string().trim().min(5).allow(''),
     streetAddress: Joi.string().trim().allow('').min(5),
     city: Joi.string().trim().required(),
     state: Joi.string().trim().length(2).required(),
-    zip: Joi.string().trim().min(5),
-    lat: Joi.number(),
-    'long': Joi.number(),
+    zip: Joi.string().trim().min(5).allow(''),
+    lat: Joi.number().allow(''),
+    'long': Joi.number().allow(''),
   }),
-  resultsUrl: Joi.string().uri(),
-  websiteUrl: Joi.string().uri(),
-  registrationUrl: Joi.string().uri(),
-  flyerUrl: Joi.string().uri(),
+  resultsUrl: Joi.string().uri().allow(''),
+  websiteUrl: Joi.string().uri().allow(''),
+  registrationUrl: Joi.string().uri().allow(''),
+  flyerUrl: Joi.string().uri().allow(''),
   flyer: Joi.object().keys({
     name: Joi.string().min(5).allow(''),
-    url: Joi.string().uri().required(),
+    url: Joi.string().uri().required().allow(''),
     mimeType: Joi.any().valid([
       'application/pdf',
       'image/jpeg',
-    ])
+    ]).allow('')
   }),
-  status: Joi.any().valid(values(Statuses)),
+  status: Joi.any().valid(values(Statuses)).allow(''),
   cancelationReason: Joi.string().min(5).allow(''),
   // if event status is "Moved" containes event id of the event to which original event is moved to
   movedToEventId: Joi.string().min(5),
   group: Joi.any().valid(['1', '2', '3', '4', '5', '']),
   draftNotes: Joi.string().min(5),
-  isDraft: Joi.boolean().required(),
+  isDraft: Joi.boolean(),
   promoters: Joi.array().items(Joi.object().keys({
     id: Joi.string().required(),
     name: Joi.string().min(5).required(),
@@ -55,13 +55,13 @@ const schema = Joi.object().keys({
     contactInfo: Joi.string().allow(''),
   })),
   // information from promoter or extended promoter information
-  promoterInfo: Joi.string(),
+  promoterInfo: Joi.string().allow(''),
   series: Joi.array().items(Joi.object().keys({
     id: Joi.string().required(),
     name: Joi.string().required(),
     // short name of the series that can be used in narrow UI contexts
     shortName: Joi.string(),
-    url: Joi.string().uri()
+    url: Joi.string().uri().allow('')
   })),
 
 })
