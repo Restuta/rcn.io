be34af5dd7706354e864bbe7c8442faf7fd206db
restuta <restuta8@gmail.com>
2017-08-04 14:36:09 -0700

    exclude Dev route from production bundle

--- src/client/routes.js
+++ src/client/routes.js
@@ -2,23 +2,32 @@ import React from 'react'
 import { Route, IndexRoute, Redirect } from 'react-router'
 import App from 'App'
 import Home from 'Home'
-import Dev from 'Dev'
 import MtbCalendar from 'calendar/MtbCalendar'
 import NcncaCalendar from 'calendar/NcncaCalendar'
 import NcncaDraftCalendar from 'calendar/NcncaDraftCalendar'
 import EventDetails from 'calendar/events/event-details/EventDetails.jsx'
 
-const routes  = (
-  <Route path="/" component={App}>
-    <IndexRoute component={Home} />
-    <Route path="/dev" component={Dev} />
-    <Route path="/calendars/norcal-mtb" component={MtbCalendar} />
-    <Route path="/calendars/ncnca-2017-draft" component={NcncaDraftCalendar} />
-    <Route path="/calendars/:calendarId" component={NcncaCalendar} />
-    <Route path="/events/:eventId" component={EventDetails}/>
-    <Redirect from="/mtb" to="/calendars/norcal-mtb" />
-    <Route path="*" component={Home}/>
-  </Route>
-)
+/* eslint-disable react/jsx-key */
+const productionRoutes = [
+  <IndexRoute component={Home} />,
+  <Route path="/calendars/norcal-mtb" component={MtbCalendar} />,
+  <Route path="/calendars/ncnca-2017-draft" component={NcncaDraftCalendar} />,
+  <Route path="/calendars/:calendarId" component={NcncaCalendar} />,
+  <Route path="/events/:eventId" component={EventDetails}/>,
+  <Redirect from="/mtb" to="/calendars/norcal-mtb" />,
+  <Route path="*" component={Home}/>,
+]
+
+const allRoutes = __ENV.Dev
+  ? [<Route path="/dev" component={require('Dev').default} />]
+    .concat(productionRoutes)
+  : productionRoutes
+
+/* eslint-enable react/jsx-key */
+
+// similar to defining route separately  like <Route path="/" component={App}>
+// but allows to dynamically create prod/dev routes more easely
+const routes = React.createElement(Route, { path: '/', component: App}, ...allRoutes)
+
 
 export default routes
