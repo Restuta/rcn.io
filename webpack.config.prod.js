83b0e289fba35c4544c6c9eb6f6f84e67b151ab0
restuta <restuta8@gmail.com>
2018-03-04 20:02:14 -0800

    update UglifyJsWebpack plugin to new version

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,14 +2,16 @@ const path = require('path')
 const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 const consts = require('./webpack/constants')
 
 const getConfig = require('./webpack/common-config').getConfig
 const commonConfig = getConfig('prod')
 
-const extractCss = new ExtractTextPlugin('[name].css', {allChunks: true})
-const htmlWebpackMinifyConfig = { // Minifying it while it is parsed
+const extractCss = new ExtractTextPlugin('[name].css', { allChunks: true })
+const htmlWebpackMinifyConfig = {
+  // Minifying it while it is parsed
   removeComments: true,
   collapseWhitespace: true,
   removeRedundantAttributes: true,
@@ -19,7 +21,7 @@ const htmlWebpackMinifyConfig = { // Minifying it while it is parsed
   keepClosingSlash: true,
   minifyJS: true,
   minifyCSS: true,
-  minifyURLs: true
+  minifyURLs: true,
 }
 
 const config = {
@@ -31,15 +33,9 @@ const config = {
   debug: false,
 
   entry: {
-    app: [
-      path.join(consts.SRC_DIR, 'client/index.js')
-    ],
-    widgets: [
-      path.join(consts.SRC_DIR, 'client/widgets/index.js')
-    ],
-    admin: [
-      path.join(consts.SRC_DIR, 'client/admin/index.js')
-    ],
+    app: [path.join(consts.SRC_DIR, 'client/index.js')],
+    widgets: [path.join(consts.SRC_DIR, 'client/widgets/index.js')],
+    admin: [path.join(consts.SRC_DIR, 'client/admin/index.js')],
     vendor: commonConfig.entry.vendor.concat([
       path.join(consts.SRC_DIR, 'client/styles/bootstrap.scss'),
     ]),
@@ -47,20 +43,19 @@ const config = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: '[name].bundle.js',
-    publicPath: '/'
+    publicPath: '/',
   },
   plugins: [
-
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.NoErrorsPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': JSON.stringify('production')
+        NODE_ENV: JSON.stringify('production'),
       },
-      '__ENV' : {
-        'Prod': true,
-        'Dev': false
-      }
+      __ENV: {
+        Prod: true,
+        Dev: false,
+      },
     }),
     //order of chunks is important, first we define common chunks between app and widgets
     // new webpack.optimize.CommonsChunkPlugin({
@@ -86,19 +81,8 @@ const config = {
 
     new webpack.optimize.DedupePlugin(),
     extractCss,
-    new webpack.optimize.UglifyJsPlugin({
-      minimize: true,
-      compressor: {
-        screw_ie8: true, // eslint-disable-line camelcase
-        warnings: false
-      },
-      mangle: {
-        screw_ie8: true // eslint-disable-line camelcase
-      },
-      output: {
-        comments: false,
-        screw_ie8: true // eslint-disable-line camelcase
-      }
+    new UglifyJsPlugin({
+      parallel: true,
     }),
     new HtmlWebpackPlugin({
       filename: consts.INDEX_HTML,
@@ -111,7 +95,7 @@ const config = {
       env: {
         Widget: false,
         Prod: true,
-        Dev: false
+        Dev: false,
       },
       minify: htmlWebpackMinifyConfig,
     }),
@@ -127,7 +111,7 @@ const config = {
       env: {
         Widget: true,
         Prod: true,
-        Dev: false
+        Dev: false,
       },
       minify: htmlWebpackMinifyConfig,
     }),
@@ -143,48 +127,49 @@ const config = {
       env: {
         Admin: true,
         Prod: true,
-        Dev: false
+        Dev: false,
       },
       minify: htmlWebpackMinifyConfig,
-    })
-
+    }),
   ],
   resolve: {
     root: [
       //path.resolve(__dirname, 'src/'),
       path.resolve(__dirname, 'src/client'),
-      path.resolve(__dirname, 'src/')
+      path.resolve(__dirname, 'src/'),
     ],
     //tells webpack to use static file when import React from 'react' is used
-    alias: commonConfig.resolve.alias
+    alias: commonConfig.resolve.alias,
   },
   module: {
     /* tells webpack to skip parsing following libraries
      requires use of "import loader" for certain modules, based on https://github.com/christianalfoni/react-webpack-cookbook/issues/30
     */
     noParse: commonConfig.module.noParse,
-    loaders: commonConfig.module.loaders.concat([{
-      test: /\.scss$/,
-      loaders: ['style', extractCss.extract('css!postcss!sass')],
-      //loaders: ['style', 'css?localIdentName=[name]_[local]_[hash:base64:3]', 'sass'],
-      exclude: /node_modules/,
-      include: path.join(consts.SRC_DIR, 'client')
-    }])
+    loaders: commonConfig.module.loaders.concat([
+      {
+        test: /\.scss$/,
+        loaders: ['style', extractCss.extract('css!postcss!sass')],
+        //loaders: ['style', 'css?localIdentName=[name]_[local]_[hash:base64:3]', 'sass'],
+        exclude: /node_modules/,
+        include: path.join(consts.SRC_DIR, 'client'),
+      },
+    ]),
   },
   //required to have proper rem to px calcualtion, default floating point precision is not enough
   //since most browsers use 15, SASS only uses 5, this leads to calculated size in px like 38.0001px
   sassLoader: {
     precision: 15,
-    includePaths: [path.join(consts.SRC_DIR, 'client/styles')]
+    includePaths: [path.join(consts.SRC_DIR, 'client/styles')],
   },
   postcss: function() {
     return [
       require('autoprefixer')({
         remove: true, //enabling of removal of outdated prefixes, just in case
-        browsers: ['last 2 versions']
-      })
+        browsers: ['last 2 versions'],
+      }),
     ]
-  }
+  },
 }
 
 module.exports = config
